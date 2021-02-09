27ab001296c6af027f3a1141cfd8bf9f4c665fac
Marco_Tulio <marcotuliomarcelinocf@gmail.com>
2021-02-09 10:30:25 -0300

    Adicionado tabela de colaboracao

--- backend/info-city/src/main/java/com/infocity/api/model/Usuario.java
+++ backend/info-city/src/main/java/com/infocity/api/model/Usuario.java
@@ -1,5 +1,7 @@
 package com.infocity.api.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -25,26 +27,32 @@ public class Usuario {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
+	@Column(name = "id", nullable = false)
 	private int id;
 	
-	@Column(name = "nome")
+	@Column(name = "nome", nullable = false)
 	@Length(min = 5, message = "*Seu Nome de Usuário deve ter pelo menos 5 characteres")
 	@NotEmpty(message = "*Por favor digite um Nome de Usuário")
 	private String nome;
 	
-	@Column(name = "email")
+	@Column(name = "email", nullable = false)
 	@Email(message = "*Por favor digite um email válido")
 	@NotEmpty(message = "*Por favor digite um email")
 	private String email;
 	
-	@Column(name = "password")
+	@Column(name = "password", nullable = false)
     @Length(min = 5, message = "*Sua password deve ter mais de 5 characteres")
     @NotEmpty(message = "*Por favor digite sua password")
 	private String password;
 	
-	@Column(name = "foto")
+	@Column(name = "foto", nullable = false)
 	private String foto;
+	
+	@Column(name="created_at", nullable = false)
+	private Date created_at;
+	
+	@Column(name="updated_at", nullable = false)
+	private Date updated_at;
 	
 	
 }
