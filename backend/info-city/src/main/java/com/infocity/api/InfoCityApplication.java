a69a0d9d43261b324313c1361996dcf32a56b5cc
Marco_Tulio <marcotuliomarcelinocf@gmail.com>
2021-02-09 09:35:25 -0300

    Criado projeto frontend

---
+++ backend/info-city/src/main/java/com/infocity/api/InfoCityApplication.java
@@ -0,0 +1,13 @@
+package com.infocity.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoCityApplication.class, args);
+	}
+
+}
