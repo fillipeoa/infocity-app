4b63421fd32d66fb4a133aa12778a8a581d4bdc8
Marco_Tulio <marcotuliomarcelinocf@gmail.com>
2021-03-28 16:10:20 -0300

    Finalizado interfaces

--- frontend/info-city/src/app/interfaces/estado/estado.ts
+++ frontend/info-city/src/app/interfaces/estado/estado.ts
@@ -1,2 +1,5 @@
 export interface Estado {
+    id: number;
+    nome: String;
+    abreviacao: String;
 }
--- frontend/info-city/src/app/interfaces/usuario/usuario.ts
+++ frontend/info-city/src/app/interfaces/usuario/usuario.ts
@@ -1,2 +1,14 @@
+import { Cidade } from "../cidade/cidade";
+import { Role } from "../role/role";
+
 export interface Usuario {
+    id: number;
+    nome: String;
+    userName: String;
+    passWord: String;
+    email: String;
+    cidade: Cidade;
+    created_at: Date;
+    updated_at: Date;
+    role: Role;
 }
