09ed68386fa29789c03c4d92cff617006d2694d7
Marco Túlio Marcelino <marco@pop-os.localdomain>
2021-03-30 08:02:10 -0300

    Finalizado pagina index

--- frontend/info-city/src/app/pages/index/index.page.ts
+++ frontend/info-city/src/app/pages/index/index.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  irParaLogin(){
+    this.router.navigateByUrl("/login");
+  }
+
+  irParaCadastrar(){
+    this.router.navigateByUrl("/cadastro-dadospessoais");
   }
 
 }
